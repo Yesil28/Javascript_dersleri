5d758f7bbd7088ee3151024bb6300c156757030a
samed yesil <https://sametyesil61@bitbucket.org/sametyesil61/bibucket_deneme.git>
2020-05-16 23:32:44 +0300

    soru6

---
+++ DOM_Calisma_Cevaplari_3/soru_6/js.js
@@ -0,0 +1,23 @@
+
+const sepet = document.getElementById("sepet");
+
+let index = 0;
+function kart1(){
+
+        
+        index+=1;
+        sepet.textContent=index;
+    
+}
+
+function kart2(){
+
+
+    index+=1;
+        sepet.textContent=index;
+}
+function kart3(){
+
+        index+=1;
+        sepet.textContent=index;
+}
