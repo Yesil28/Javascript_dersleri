a0e57fa189bdaada3339d698acdcfded5865bb13
samed yesil <https://sametyesil61@bitbucket.org/sametyesil61/bibucket_deneme.git>
2020-05-18 17:03:57 +0300

    1.soru yapıldı

--- DOM_Calisma_Cevaplari_3/soru_1/js.js
+++ DOM_Calisma_Cevaplari_3/soru_1/js.js
@@ -1,33 +1,66 @@
 
-var sifre = document.getElementsByClassName("form-control")[1];
-var mail = document.getElementsByClassName("form-control")[0];
+var sifre = document.getElementById("sifre");
+var mail = document.getElementById("mail");
 
-
-
-var form1 = document.getElementById("form1");
-var form2 = document.getElementById("form2");
+var buton = document.getElementById("buton");
 
 function tikla() {
-    var etiket_ac;
-    let a;
 
-    if(mail.){
+    mail.textContent="İl";
+    sifre.textContent = "ilçe";
 
-    }
+
+    var x = "Mahalle";
+    document.getElementById("demo").innerHTML = x;
+
+    var y = "Cadde";
+    document.getElementById("x").innerHTML = y;
+
+
+
+    var abc=document.createElement("input");
+            
+    abc.setAttribute("type","text");
+    abc.setAttribute("class","form-control");
     
+    var panelDivv=document.getElementById("y");
+    panelDivv.appendChild(abc);
 
 
-    if (mail.value == "" || sifre.value == "") {
 
-        a = form1.children[1];
-        etiket_ac = document.createElement("span");
-        etiket_ac.id = "hata";
-        etiket_ac.appendChild(document.createTextNode("boş bırakılmaz"));
-        a.appendChild(etiket_ac);
+    var nesne=document.createElement("input");
+            
+    nesne.setAttribute("type","text");
+    nesne.setAttribute("class","form-control");
+    
+    var panelDiv=document.getElementById("panel");
+    panelDiv.appendChild(nesne);
+
+    buton.remove();
+
+
+
+
+    var butonn=document.createElement("input");
+            
+    butonn.setAttribute("type","button");
+    butonn.setAttribute("class","btn btn-primary w-100 mt-5");
+    butonn.setAttribute("id","yeni_buton");
+    butonn.setAttribute("value","İlet");
+
+    
+    var butonnn=document.getElementById("panel");
+    butonnn.appendChild(butonn);
+
+
+    const yeni_butonn = document.querySelector("#yeni_buton");
+
+    yeni_butonn.addEventListener("click",run);
+
+
+    
+    function run(e){
+        alert("iletildi");
     }
-    else {
-        form2.open
-    }
-
 }
 
